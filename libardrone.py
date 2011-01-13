8839451916b79021d9e9a4625eac14b464510475
Bastian Venthur <venthur@debian.org>
2011-01-13 18:57:33 +0100

    Started to add various low level at commands.

---
+++ libardrone.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2011 Bastian Venthur
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+"""
+Docstring goes here.
+"""
+
+
+import struct
+
+
+__author__ = "Bastian Venthur"
+
+###############################################################################
+### Low level AT Commands
+###############################################################################
+
+def at_ref(seq, takeoff, emergency=False):
+    """
+    Basic behaviour of the drone: take-off/landing, emergency stop/reset)
+
+    Parameters:
+    seq -- sequence number
+    takeoff -- True: Takeoff / False: Land
+    emergency -- True: Turn of the engines
+    """
+    p = 0b1000000000 if takeoff else 0b0
+    if emergency:
+        p += 0b0100000000
+    at("REF", seq, [p])
+
+def at_pcmd(seq, progressive, lr, fb, vv, va):
+    """
+    Makes the drone move (translate/rotate).
+
+    Parameters:
+    seq -- sequence number
+    progressive -- True: enable progressive commands, False: disable (i.e.
+        enable hovering mode)
+    lr -- left-right tilt: float [-1..1] negative: left, positive: right
+    rb -- front-back tilt: float [-1..1] negative: forwards, positive:
+        backwards
+    vv -- vertical speed: float [-1..1] negative: go down, positive: rise
+    va -- angular speed: float [-1..1] negative: spin left, positive: spin 
+        right
+
+    The above float values are a percentage of the maximum speed.
+    """
+    p = 1 if progressive else 0
+    at("PCMD", seq, [p, lr, fb, vv, va])
+
+def at_ftrim(seq):
+    """
+    Tell the drone it's lying horizontally.
+
+    Parameters:
+    seq -- sequence number
+    """
+    at("FTRIM", seq)
+
+def at_zap(seq, stream):
+    """
+    Selects which video stream to send on the video UDP port.
+
+    Parameters:
+    seq -- sequence number
+    stream -- Integer: video stream to broadcast
+    """
+    # FIXME: improve parameters to select the modes directly
+    at("ZAP", seq, [stream])
+
+def at_config(seq, option, value):
+    pass
+
+def at_comwdg():
+    """
+    Reset communication watchdog.
+    """
+    # FIXME: no sequence number
+    pass
+
+def at(command, seq, params=[]):
+    """
+    Parameters:
+    command -- the command
+    seq -- the sequence number
+    params -- a list of elements which can be either int, float or string
+    """
+    param_str = ''
+    for p in params:
+        if type(p) == int:
+            param_str += ",%d" % p
+        elif type(p) == float:
+            param_str += ",%d" % f2i(p)
+        elif type(p) == str:
+            param_str += ',"'+p+'"'
+    print "AT*%s=%i%s\n" % (command, seq, param_str)
+
+def f2i(f):
+    """Interpret IEEE-754 floating-point value as signed integer.
+
+    Arguments:
+    f -- floating point value
+    """
+    return struct.unpack('i', struct.pack('f', f))[0]
+
