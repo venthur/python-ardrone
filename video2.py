1e9cea460f714dd1bbd28dad6a970f00555f9949
Bastian Venthur <venthur@debian.org>
2011-04-13 10:58:37 +0200

    Use select and added option to break the loop by hitting enter.

--- video2.py
+++ video2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-
-import threading
+import datetime
+import select
 import socket
+import sys
+import threading
 import time
 
 import libardrone
@@ -13,7 +15,7 @@ class ARDroneVideoThread(threading.Thread):
     def run(self):
         print 'preparing server...'
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(1)
+        sock.setblocking(0)
         sock.bind(('', 5555))
         self.stopping = False
         # will the drone send one frame per udp package?
@@ -22,17 +24,32 @@ class ARDroneVideoThread(threading.Thread):
         sock.sendto("\x01\x00\x00\x00", ('192.168.1.1', 5555))
 
         print 'waiting for incoming data'
+        j = 0
         while not self.stopping:
-            print 'reading socket...',
-            data, address = sock.recvfrom(65535)    # max udp packet size
-
-            br = video.BitReader(data)
-            video.read_picture(br)
-            print 'ok.'
-            # do something with the data
-            print time.time(), len(data)
+            j += 1
+            inputready, outputready, exceptready = select.select([sock, sys.stdin], [], [])
+            for i in inputready:
+                if i == sock:
+                    print 'reading socket...',
+                    t = datetime.datetime.now()
+                    while 1:
+                        try:
+                            j += 1
+                            data, address = sock.recvfrom(65535)    # max udp packet size
+                        except:
+                            print "dropped", j-1, "frames"
+                            j = 0
+                            break
+                    br = video.BitReader(data)
+                    t_decode = video.read_picture(br)
+                    t2 = datetime.datetime.now()
+                    print 'ok.', (t2 - t).microseconds / 1000000., 'sec', t_decode
+                elif i == sys.stdin:
+                    sys.stdin.readline()
+                    print "stopping the loop"
+                    self.stopping = True
         print 'left receiving loop'
-        socket.close()
+        sock.close()
 
     def stop(self):
         print "signaling thread to stop"
@@ -41,9 +58,9 @@ class ARDroneVideoThread(threading.Thread):
 def main():
     vthread = ARDroneVideoThread()
     vthread.start()
-    time.sleep(60)
-    vthread.stop()
-
+    #time.sleep(60)
+    #vthread.stop()
+    vthread.join()
 
 if __name__ == '__main__':
     main()
