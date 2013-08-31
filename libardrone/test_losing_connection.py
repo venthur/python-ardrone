5161779c211ff0fc3904df30e75e66f39cc60a8b
Jean-Baptiste Passot <passot@braincorporation.com>
2013-08-30 18:09:11 -0700

    new test for losing connection

---
+++ libardrone/test_losing_connection.py
@@ -0,0 +1,70 @@
+import select
+import socket
+import struct
+import time
+
+DRONE_IP = "192.168.1.1"
+ARDRONE_NAVDATA_PORT = 5554
+ARDRONE_COMMAND_PORT = 5556
+
+def at(command, seq, params):
+    """
+    Parameters:
+    command -- the command
+    seq -- the sequence number
+    params -- a list of elements which can be either int, float or string
+    """
+    param_str = ''
+    for p in params:
+        if type(p) == int:
+            param_str += ",%d" % p
+        elif type(p) == float:
+            param_str += ",%d" % f2i(p)
+        elif type(p) == str:
+            param_str += ',"' + p + '"'
+    msg = "AT*%s=%i%s\r" % (command, seq, param_str)
+    print msg
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(msg, ("192.168.1.1", ARDRONE_COMMAND_PORT))
+
+def f2i(f):
+    """Interpret IEEE-754 floating-point value as signed integer.
+    Arguments:
+    f -- floating point value
+    """
+    return struct.unpack('i', struct.pack('f', f))[0]
+
+
+if __name__ == '__main__':
+    nav_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    # That
+    nav_socket.connect((DRONE_IP, ARDRONE_NAVDATA_PORT))
+    nav_socket.setblocking(0)
+    nav_socket.send("\x01\x00\x00\x00")
+    # or that...
+    nav_socket.setblocking(0)
+    nav_socket.bind(('', ARDRONE_NAVDATA_PORT))
+    nav_socket.sendto("\x01\x00\x00\x00", ('192.168.1.1', ARDRONE_NAVDATA_PORT))
+
+    #nav_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    #nav_socket.connect((DRONE_IP, ARDRONE_NAVDATA_PORT))
+    #nav_socket.settimeout(1)
+    #nav_socket.send("\x01\x00\x00\x00")
+
+    seq = 1
+    stopping = False
+    while not stopping:
+        inputready, outputready, exceptready = select.select([nav_socket], [], [], 20)
+        seq += 1
+        at("COMWDG", seq, [])
+        if len(inputready) == 0:
+            stopping = True
+        for i in inputready:
+            while 1:
+                try:
+                    data = nav_socket.recv(500)
+                except IOError:
+                    break
+                print time.time(), " ", len(data)
+
+    raise Exception("Should not get here")
