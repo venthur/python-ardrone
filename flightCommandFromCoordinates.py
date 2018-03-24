f7fbffb3444fe82f0c8138230c056f60a75ecc20
nander <nanderv@gmail.com>
2018-03-24 16:29:16 +0100

    Values updated to make the thing fly slower.

--- flightCommandFromCoordinates.py
+++ flightCommandFromCoordinates.py
@@ -7,11 +7,11 @@ def get_flight_command(offset):
 
     global notFoundCounter
     if offset is None:
-        if notFoundCounter < 20:
+        if notFoundCounter < 3:
             notFoundCounter += 1
-            return 0, 0, 0, 0
+            return 0, -0.2, 0.5, 0
         else:
             return None, None, None, None
     notFoundCounter = 0
 
-    return -offset[0], 0.5, -offset[1], 0
+    return -offset[0], -0.2, -offset[1], 0
