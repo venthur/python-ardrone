70e63604c418602a33fe60e1972d7ead6e3f1c5e
nander <nanderv@gmail.com>
2018-03-24 12:07:23 +0100

    Simplified program to test if hover can be averted.

--- demo.py
+++ demo.py
@@ -32,7 +32,6 @@ import pygame
 
 import libardrone
 
-
 def main():
     pygame.init()
     W, H = 320, 240
@@ -44,8 +43,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False 
-            elif event.type == pygame.KEYUP:
-                drone.hover()
+
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     drone.reset()
@@ -53,57 +51,18 @@ def main():
                 # takeoff / land
                 elif event.key == pygame.K_RETURN:
                     drone.takeoff()
+                    drone.at(libardrone.at_pcmd, True, 0, 0, 1, 1)
                 elif event.key == pygame.K_SPACE:
                     drone.land()
-                # emergency
-                elif event.key == pygame.K_BACKSPACE:
-                    drone.reset()
-                # forward / backward
-                elif event.key == pygame.K_w:
-                    drone.move_forward()
-                elif event.key == pygame.K_s:
-                    drone.move_backward()
-                # left / right
-                elif event.key == pygame.K_a:
-                    drone.move_left()
-                elif event.key == pygame.K_d:
-                    drone.move_right()
-                # up / down
-                elif event.key == pygame.K_UP:
-                    drone.move_up()
-                elif event.key == pygame.K_DOWN:
-                    drone.move_down()
-                # turn left / turn right
-                elif event.key == pygame.K_LEFT:
-                    drone.turn_left()
-                elif event.key == pygame.K_RIGHT:
-                    drone.turn_right()
-                # speed
-                elif event.key == pygame.K_1:
-                    drone.speed = 0.1
-                elif event.key == pygame.K_2:
-                    drone.speed = 0.2
-                elif event.key == pygame.K_3:
-                    drone.speed = 0.3
-                elif event.key == pygame.K_4:
-                    drone.speed = 0.4
-                elif event.key == pygame.K_5:
-                    drone.speed = 0.5
-                elif event.key == pygame.K_6:
-                    drone.speed = 0.6
-                elif event.key == pygame.K_7:
-                    drone.speed = 0.7
-                elif event.key == pygame.K_8:
-                    drone.speed = 0.8
-                elif event.key == pygame.K_9:
-                    drone.speed = 0.9
-                elif event.key == pygame.K_0:
-                    drone.speed = 1.0
+                elif event.type == pygame.K_UP:
+                    drone.hover()
+                elif event.type == pygame.K_DOWN:
+                    drone.at(libardrone.at_pcmd, True, 0, 0, 1, -1)
 
         try:
             surface = pygame.image.fromstring(drone.image, (W, H), 'RGB')
             # battery status
-            hud_color = (255, 0, 0) if drone.navdata.get('drone_state', dict()).get('emergency_mask', 1) else (10, 10, 255)
+            hud_color = (255, 0,  0) if drone.navdata.get('drone_state', dict()).get('emergency_mask', 1) else (10, 10, 255)
             bat = drone.navdata.get(0, dict()).get('battery', 0)
             f = pygame.font.Font(None, 20)
             hud = f.render('Battery: %i%%' % bat, True, hud_color)
