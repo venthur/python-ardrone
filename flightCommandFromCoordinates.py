4883bd3eb4d92c9e037522d51073dafd36866006
nander <nanderv@gmail.com>
2018-03-24 17:24:01 +0100

    This may add startcounter.

--- flightCommandFromCoordinates.py
+++ flightCommandFromCoordinates.py
@@ -1,18 +1,21 @@
 import math
 
 notFoundCounter = -1000000
+startCounter = 0
 
 
 def get_flight_command(offset):
     global notFoundCounter
+    global startCounter
     if offset is None or offset[0] * offset[0] + offset[1] * offset[1] > 0.6:
-        if notFoundCounter < 20:
+        if startCounter > 30 and notFoundCounter < 20:
             notFoundCounter += 1
             print(" NOT FOUND {}".format(notFoundCounter))
             return 0, -0.2, 0.5, 0
         else:
             print("LANDING")
             return None, None, None, None
+    startCounter += 1
     notFoundCounter = 0
 
-    return offset[0], -0.2, -offset[1], 0
+    return 0.5*offset[0], -0.2, 0.1-offset[1], 0.2*offset[0]
