6b2c9d2245c7ee61894a2083431493751f355c87
nander <nanderv@gmail.com>
2018-03-24 13:01:38 +0100

    Should be working code.

--- demo.py
+++ demo.py
@@ -1,83 +1,62 @@
-#!/usr/bin/env python
 
-# Copyright (c) 2011 Bastian Venthur
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# !/usr/bin/env python
 
-
-"""Demo app for the AR.Drone.
-
-This simple application allows to control the drone and see the drone's video
-stream.
-"""
-
-
+import cv2
 import pygame
-
+import numpy
 import libardrone
 
-def main():
-    pygame.init()
-    W, H = 320, 240
-    screen = pygame.display.set_mode((W, H))
-    drone = libardrone.ARDrone()
-    clock = pygame.time.Clock()
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False 
+running = True
+flying = False
+path = 'tcp://192.168.1.1:5555'
 
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    drone.reset()
-                    running = False
-                # takeoff / land
-                elif event.key == pygame.K_RETURN:
+drone = libardrone.ARDrone()
+drone.reset()
+drone.speed = 0.5
+pygame.init()
+W, H = 640, 360
+hud_color = (255, 0, 0)
+screen = pygame.display.set_mode((W, H))
+clock = pygame.time.Clock()
+stream = cv2.VideoCapture(path)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                drone.land()
+                drone.reset()
+            elif event.key == pygame.K_SPACE:
+                if flying == False:
                     drone.takeoff()
-                    drone.at(libardrone.at_pcmd, True, 0, 1, 1, 0)
-                elif event.key == pygame.K_SPACE:
+                    flying = True
+                else:
                     drone.land()
-                elif event.type == pygame.K_UP:
-                    drone.hover()
-                elif event.type == pygame.K_DOWN:
-                    drone.at(libardrone.at_pcmd, True, 0, 1, -1, 0)
+                    flying = False
+            elif event.key == pygame.K_w:
+                drone.at(libardrone.at_pcmd, True, 0, 1, 1, 0)
 
-        try:
-            surface = pygame.image.fromstring(drone.image, (W, H), 'RGB')
-            # battery status
-            hud_color = (255, 0,  0) if drone.navdata.get('drone_state', dict()).get('emergency_mask', 1) else (10, 10, 255)
-            bat = drone.navdata.get(0, dict()).get('battery', 0)
-            f = pygame.font.Font(None, 20)
-            hud = f.render('Battery: %i%%' % bat, True, hud_color)
-            screen.blit(surface, (0, 0))
-            screen.blit(hud, (10, 10))
-        except:
-            pass
-
+    if running == False:
+        print "Shut Down"
+        pygame.quit()
+        drone.halt()
+        break
+    try:
+        buff = stream.grab()
+        imageyuv = stream.retrieve(buff)
+        imagergb = cv2.cvtColor(imageyuv[1], cv2.COLOR_BGR2RGB)
+        surface = pygame.image.frombuffer(imagergb, (W, H), 'RGB')
+        bat = drone.navdata.get('battery', 0)
+        screen.blit(surface, (0, 0))
         pygame.display.flip()
         clock.tick(50)
         pygame.display.set_caption("FPS: %.2f" % clock.get_fps())
-
-    print "Shutting down...",
-    drone.halt()
-    print "Ok."
-
-if __name__ == '__main__':
-    main()
+        f = pygame.font.Font(None, 20)
+        hud = f.render('Battery: %i%%' % bat, True, hud_color)
+        print bat
+        screen.blit(hud, (10, 10))
+    except:
+        print "Missed Frame"
+        pass
+pygame.quit()
