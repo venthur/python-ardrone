9705bdaa5979136230402b5663a706d43096b7b5
nander <nanderv@gmail.com>
2018-03-24 16:35:09 +0100

    Updated to ignore values outside center.

--- flightCommandFromCoordinates.py
+++ flightCommandFromCoordinates.py
@@ -8,8 +8,10 @@ def get_flight_command(offset):
     if offset is None or offset.x * offset.x + offset.y * offset.y > 0.6:
         if notFoundCounter < 3:
             notFoundCounter += 1
+            print(" NOT FOUND" + notFoundCounter)
             return 0, -0.2, 0.5, 0
         else:
+            print("LANDING")
             return None, None, None, None
     notFoundCounter = 0
 
